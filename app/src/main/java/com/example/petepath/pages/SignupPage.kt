3e7412e9e6f0e84507b83fc8bfd233d43814003c
EkaPutri <adifa.note2@gmail.com>
2024-11-17 16:14:52 +0800

    update report page and signup page

--- app/src/main/java/com/example/petepath/pages/SignupPage.kt
+++ app/src/main/java/com/example/petepath/pages/SignupPage.kt
@@ -1,6 +1,5 @@
 package com.example.petepath.pages
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,11 +9,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,9 +20,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -127,8 +121,7 @@ fun SignupPage(modifier: Modifier = Modifier, navController: NavController? = nu
             onClick = { /* Handle click */ },
             modifier = Modifier
                 .width(275.dp)
-                .height(56.dp)
-                , // Atur border secara manual
+                .height(56.dp), // Atur border secara manual
             shape = RoundedCornerShape(12.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = outlineColor, // Background putih
