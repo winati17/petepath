fa56ae00a37ec02e984e14b5bff7085a53558ff1
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2024-11-27 01:24:21 +0800

    add profile page

--- app/src/main/java/com/example/petepath/pages/features/HistoryActivity.kt
+++ app/src/main/java/com/example/petepath/pages/features/HistoryActivity.kt
@@ -1,12 +1,10 @@
 package com.example.petepath.pages.features
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,29 +14,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterStart
 import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-//import androidx.compose.ui.text.font.Font
-//import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.petepath.R
-import com.example.petepath.ui.theme.HistoryIcon
-import com.example.petepath.ui.theme.HomepageIcon
-import com.example.petepath.ui.theme.ProfileIcon
-import com.example.petepath.ui.theme.ReportIcon
 
 @Composable
 fun HistoryActivity(){
@@ -194,19 +182,7 @@ fun HistoryActivity(){
         }
     }
 
-//    KENAPA MALAH DI ATAS INI BEJIR
-//    BottomAppBar(
-//        contentColor = mainColor,
-//        containerColor = Color.White
-//    ) {
-//        HomepageIcon()
-//        Spacer(modifier = Modifier.weight(1f))
-//        HistoryIcon(active = true)
-//        Spacer(modifier = Modifier.weight(1f))
-//        ReportIcon()
-//        Spacer(modifier = Modifier.weight(1f))
-//        ProfileIcon()
-//    }
+//    bottomappbar di sini...
 }
 
 @Preview(showBackground = true)
