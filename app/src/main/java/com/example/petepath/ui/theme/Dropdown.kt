7acebad3cb31ab9aa4a0247225297fb5fa3b5a58
winati17 <winatimn@gmail.com>
2024-12-11 17:52:23 +0800

    refactor : dropdown composable

---
+++ app/src/main/java/com/example/petepath/ui/theme/Dropdown.kt
@@ -0,0 +1,94 @@
+package com.example.petepath.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DropdownMenuField(
+    label: String,
+    options: List<String>,
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = selectedOption,
+            onValueChange = {},
+            readOnly = true,
+            label = { Text(label) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp, horizontal = 10.dp),
+            trailingIcon = {
+                IconButton(onClick = { expanded = !expanded }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Dropdown icon",
+                        tint = Color(0xFF007BFF) // Adjust color to match the design
+                    )
+                }
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                focusedLabelColor = Color(0xFF007BFF),
+                unfocusedLabelColor = Color.Gray,
+                unfocusedTrailingIconColor = Color(0xFF007BFF),
+                focusedBorderColor = Color(0xFF007BFF),
+                unfocusedBorderColor = Color(0xFF007BFF),
+                cursorColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(8.dp)
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .background(Color.White, shape = RoundedCornerShape(16.dp))
+                .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+        ) {
+            options.forEachIndexed { index, option ->
+                DropdownMenuItem(
+                    onClick = {
+                        onOptionSelected(option)
+                        expanded = false
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    text = { Text(option, style = TextStyle(color = Color.Black, fontSize = 14.sp)) },
+                )
+                if (index != options.lastIndex) {
+                    HorizontalDivider(color = Color.Gray, thickness = 1.dp) // Divider for all items except the last one
+                }
+            }
+        }
+
+    }
+}
